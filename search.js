364f69826d8e058cf71ff7078afe09f7ac8c25d9
Austin Brown <austin@alumino.us>
2014-04-24 21:36:27 -0600

    Add layout

---
+++ search.js
@@ -0,0 +1,52 @@
+function ratelimit(fn, delay) {
+   delay = delay || 300;
+
+   var delayTimer = null;
+
+   return function() {
+      var args = arguments;
+      var callerThis = this;
+      queuedCall = function(){
+         delayTimer = null;
+         fn.apply(callerThis, args)
+      };
+
+      if (delayTimer) {
+         window.clearTimeout(delayTimer);
+      }
+
+      delayTimer = window.setTimeout(queuedCall, delay);
+   };
+}
+
+function textSearch() {
+	var query = $(this).val()
+		.toLowerCase()
+		.replace(/[,.\/\\\[\]\(\)"']/, '')
+		.split(' ');
+
+	if (query) {
+		$('#transcription > *').each(function(i, el) {
+			var score = 0;
+			el = $(el);
+			var text = el.text()
+				.toLowerCase()
+				.replace(/[,.\/\\\[\]\(\)"']/, '')
+
+			for (i in query) {
+				if (!text.contains(query[i])) {
+					el.hide();
+					return;
+				}
+			}
+
+			el.show();
+		});
+	} else {
+		$('#transcription > *').show();
+	}
+}
+
+$(function(){
+	$('#search_field').on('keypress', ratelimit(textSearch, 200))
+});
